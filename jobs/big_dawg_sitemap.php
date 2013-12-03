46cee39dec19f61261424ef91e197da5ee28e55c
Mike Lay <mike@mkly.io>
2013-12-03 12:26:22 -0800

    Account for Page Attributes
    Account for page attributes pertaining to sitemap.xml
    
    - sitemap_priority
    - sitemap_changefreq
    - exclude_sitemapxml

--- jobs/big_dawg_sitemap.php
+++ jobs/big_dawg_sitemap.php
@@ -20,7 +20,57 @@ class BigDawgSitemap extends Job {
 		$prettyURL = URL_REWRITING ? '' : '/index.php';
 
 		$r = Loader::db()->Execute('
-			SELECT pp.cPath, c.cDateModified
+
+			SELECT p.cID AS cID,
+			       pp.cPath AS cPath,
+			       c.cDateModified AS cDateModified,
+			       ak.akHandle AS akHandle,
+			       cav.avID AS avID,
+			       atb.value AS booleanValue,
+			       atd.value AS defaultValue,
+			       atn.value AS numberValue
+			FROM PagePermissionAssignments ppa
+			JOIN PermissionKeys pk ON ppa.pkID = pk.pkID
+			JOIN PermissionAccess pa ON pa.paID = ppa.paID
+			JOIN PermissionAccessList pal ON pal.paID = ppa.paID
+			JOIN PermissionAccessEntityGroups peg ON peg.peID = pal.peID
+			JOIN Pages p ON p.cInheritPermissionsFromCID = ppa.cID
+			JOIN CollectionVersions cv ON cv.cID = p.cID
+			JOIN Collections c ON c.cID = cv.cID
+			JOIN PagePaths pp ON pp.cID = cv.cID
+			JOIN CollectionAttributeValues cav ON cav.cvID = cv.cvID
+			JOIN AttributeValues av ON av.avID = cav.avID
+			JOIN AttributeKeys ak ON ak.akID = cav.akID
+			JOIN AttributeTypes at ON at.atID = av.atID
+			LEFT JOIN atDefault atd ON atd.avID = cav.avID
+			LEFT JOIN atBoolean atb ON atb.avID = cav.avID
+			LEFT JOIN atNumber atn ON atn.avID = cav.avID
+			WHERE pk.pkHandle = "view_page"
+			AND pa.paIsInUse = 1
+			AND pal.accessType = 10
+			AND peg.gID = ?
+			AND p.cIsActive = 1
+			AND p.cPointerID = 0
+			AND p.cIsSystemPage = 0
+			AND cv.cvIsApproved = 1
+			AND pp.ppIsCanonical = 1
+			AND pp.cPath NOT LIKE "/dashboard%"
+			AND ak.akHandle IN (
+				"exclude_sitemapxml",
+				"sitemap_priority",
+				"sitemap_changefreq"
+			)
+
+			UNION
+
+			SELECT p.cID AS cID,
+			       pp.cPath AS cPath,
+			       c.cDateModified AS cDateModified,
+			       NULL AS akHandle,
+			       NULL as avID,
+			       NULL as booleanValue,
+			       NULL AS defaultValue,
+			       NULL AS numberValue
 			FROM PagePermissionAssignments ppa
 			JOIN PermissionKeys pk ON ppa.pkID = pk.pkID
 			JOIN PermissionAccess pa ON pa.paID = ppa.paID
@@ -40,12 +90,56 @@ class BigDawgSitemap extends Job {
 			AND cv.cvIsApproved = 1
 			AND pp.ppIsCanonical = 1
 			AND pp.cPath NOT LIKE "/dashboard%"
-		', array(GUEST_GROUP_ID));
 
-		while ($row = $r->FetchRow()) {
-			$sitemap->addItem($prettyURL . $row['cPath'], SITEMAPXML_DEFAULT_PRIORITY, SITEMAPXML_DEFAULT_CHANGEFREQ, $row['cDateModified']);
-		}
+			ORDER BY cID, akHandle DESC
+
+		', array(GUEST_GROUP_ID, GUEST_GROUP_ID));
+
+		$this->processRows($r, $sitemap);
 
 		$sitemap->createSitemapIndex(rtrim(BASE_URL, '/') . '/');
+	}
+
+	protected function processRows(&$r, &$sitemap, $cID = false, $exclude = false, $frequency = false, $priority = false) {
+
+		if (!$row = $r->FetchRow()) {
+			return;
+		}
+
+		if ($row['cID'] != $cID) {
+			$exclude = false;
+			$priority = false;
+			$frequency = false;
+			$cID = $row['cID'];
+		}
+
+		if ($row['akHandle'] == 'exclude_sitemapxml') {
+			$exclude = (bool) $row['booleanValue'];
+			$this->processRows($r, $sitemap, $cID, $exclude, $frequency, $priority);
+			return;
+		}
+		if ($row['akHandle'] == 'sitemap_priority') {
+			$priority = $row['defaultValue'] !== null ? $row['defaultValue'] : (float) number_format((float) $row['numberValue'], 2);
+			$this->processRows($r, $sitemap, $cID, $exclude, $frequency, $priority);
+			return;
+		}
+		if ($row['akHandle'] == 'sitemap_changefreq') {
+			$frequency = $row['defaultValue'];
+			$this->processRows($r, $sitemap, $cID, $exclude, $frequency, $priority);
+			return;
+		}
+
+		if (!$priority) {
+			$priority = SITEMAPXML_DEFAULT_PRIORITY;
+		}
+		if (!$frequency) {
+			$frequency = SITEMAPXML_DEFAULT_CHANGEFREQ;
+		}
+
+		if (!$exclude) {
+			$sitemap->addItem($prettyURL . $row['cPath'], $priority, $frequency, $row['cDateModified']);
+		}
+
+		$this->processRows($r, $sitemap);
 	}
 }
