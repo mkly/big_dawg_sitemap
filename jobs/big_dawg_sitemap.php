6b043f356638bdc571d5661703dbb5544f466787
Mike Lay <mike@mkly.io>
2013-12-03 16:19:15 -0800

    Account For Homepage in Sitemap
    Account For Homepage in Sitemap
    
    Sitemap did not include Home page as there is no canonical entry in the
    PagePaths table
    
    - Add two additional `UNIONS`s for just the home page

--- jobs/big_dawg_sitemap.php
+++ jobs/big_dawg_sitemap.php
@@ -91,9 +91,77 @@ class BigDawgSitemap extends Job {
 			AND pp.ppIsCanonical = 1
 			AND pp.cPath NOT LIKE "/dashboard%"
 
+			UNION
+
+			SELECT p.cID AS cID,
+			       "" AS cPath,
+			       c.cDateModified AS cDateModified,
+			       ak.akHandle AS akHandle,
+			       cav.avID AS avID,
+			       atb.value AS booleanValue,
+			       atd.value AS defaultValue,
+			       atn.value AS numberValue
+			FROM PagePermissionAssignments ppa
+			JOIN PermissionKeys pk ON ppa.pkID = pk.pkID
+			JOIN PermissionAccess pa ON pa.paID = ppa.paID
+			JOIN PermissionAccessList pal ON pal.paID = ppa.paID
+			JOIN PermissionAccessEntityGroups peg ON peg.peID = pal.peID
+			JOIN Pages p ON p.cInheritPermissionsFromCID = ppa.cID
+			JOIN CollectionVersions cv ON cv.cID = p.cID
+			JOIN Collections c ON c.cID = cv.cID
+			JOIN CollectionAttributeValues cav ON cav.cvID = cv.cvID
+			JOIN AttributeValues av ON av.avID = cav.avID
+			JOIN AttributeKeys ak ON ak.akID = cav.akID
+			JOIN AttributeTypes at ON at.atID = av.atID
+			LEFT JOIN atDefault atd ON atd.avID = cav.avID
+			LEFT JOIN atBoolean atb ON atb.avID = cav.avID
+			LEFT JOIN atNumber atn ON atn.avID = cav.avID
+			WHERE pk.pkHandle = "view_page"
+			AND pa.paIsInUse = 1
+			AND pal.accessType = 10
+			AND peg.gID = ?
+			AND p.cIsActive = 1
+			AND p.cPointerID = 0
+			AND p.cIsSystemPage = 0
+			AND cv.cvIsApproved = 1
+			AND ak.akHandle IN (
+				"exclude_sitemapxml",
+				"sitemap_priority",
+				"sitemap_changefreq"
+			)
+			AND p.cID = ?
+
+			UNION
+
+			SELECT p.cID AS cID,
+			       "" AS cPath,
+			       c.cDateModified AS cDateModified,
+			       NULL AS akHandle,
+			       NULL as avID,
+			       NULL as booleanValue,
+			       NULL AS defaultValue,
+			       NULL AS numberValue
+			FROM PagePermissionAssignments ppa
+			JOIN PermissionKeys pk ON ppa.pkID = pk.pkID
+			JOIN PermissionAccess pa ON pa.paID = ppa.paID
+			JOIN PermissionAccessList pal ON pal.paID = ppa.paID
+			JOIN PermissionAccessEntityGroups peg ON peg.peID = pal.peID
+			JOIN Pages p ON p.cInheritPermissionsFromCID = ppa.cID
+			JOIN CollectionVersions cv ON cv.cID = p.cID
+			JOIN Collections c ON c.cID = cv.cID
+			WHERE pk.pkHandle = "view_page"
+			AND pa.paIsInUse = 1
+			AND pal.accessType = 10
+			AND peg.gID = ?
+			AND p.cIsActive = 1
+			AND p.cPointerID = 0
+			AND p.cIsSystemPage = 0
+			AND cv.cvIsApproved = 1
+			AND p.cID = ?
+
 			ORDER BY cID, akHandle DESC
 
-		', array(GUEST_GROUP_ID, GUEST_GROUP_ID));
+		', array(GUEST_GROUP_ID, GUEST_GROUP_ID, GUEST_GROUP_ID, HOME_CID, GUEST_GROUP_ID, HOME_CID));
 
 		$this->processRows($r, $sitemap);
 
